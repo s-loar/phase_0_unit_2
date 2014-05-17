1bc33d726a1224a371d68f682635e5feda2146dc
Steve Loar <sjl7909@gmail.com>
2014-05-17 18:53:56 -0400

    Week #4 Exercise #1

--- week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
+++ week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
@@ -4,22 +4,69 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 1. Pseudocode
 
-# What is the input?
+# What is the input? An array
 # What is the output? (i.e. What should the code return?)
+# total: the sum of the numbers in the array
+# sentence_maker: A sentence from the array wit hthe first word capitalized and a period at the end.
 # What are the steps needed to solve the problem?
+# total
+# define a variable to hold the sum
+# for each element in the array
+# add it to the total
+# return the total
+# 
+# sentence_maker
+# define a variable to hold the sentence 
+# loop through the array of words
+# if it is the first word in the sentence, capitalize the first letter and add it to the phrase
+# otherwise add a space and the word to the sentence
+# return the sentence plus a period at the end
+
 
 
 # 2. Initial Solution
+# def total( nums )
+# 	sum = 0
+# 	nums.each do |number|
+# 		sum += number
+# 	end
+# 	return sum
+# end
 
-
+def sentence_maker( words ) 
+	phrase = ""
+	words.each do |word|
+		if phrase.length > 0 then
+			phrase += " " + word.to_s
+		else
+			phrase += word.to_s.capitalize
+		end
+	end
+	return phrase + "."
+end
 
 
 # 3. Refactored Solution
+def total( nums )
+	return nums.inject(0) { |total, num| total + num }
+end 
 
+
+# Driver code from the problem
+# total([1,2,3])      # => 6
+# total([4.5, 0, -1]) # => 3.5
+# total([-100, 100])  # => 0
+
+# sentence_maker(["i", "want", "to", "go", "to", "the", "movies"]) # => "I want to go to the movies."
 
 
 # 4. Reflection 
+# These were fun exercises to do. Initialy I just tried to follow the pseudocode and adjust things
+# to get the rspec tests to pass. I got to apply a new method, inject, which I picked up in
+# another exercise this week. I liked how I could do the total function in one line that was 
+# also easy to read.  I feel confident that I learned the compentenies of this exercise.
+
--- week_4/1_mathy_ruby_intro/hard_find_mode/my_solution.rb
+++ week_4/1_mathy_ruby_intro/hard_find_mode/my_solution.rb
@@ -8,18 +8,48 @@
 
 # 1. Pseudocode
 
-# What is the input?
+# What is the input? an array of numbers or strings
 # What is the output? (i.e. What should the code return?)
+# an array of the highest occuring value(s) in the array 
 # What are the steps needed to solve the problem?
+# create a hash of the array values and the number of times it occurs in the array
+# create an array of the keys with the highest value 
+# sort that array
+# return the array
 
 
 # 2. Initial Solution
+def mode( array )
+  # create a hash of the arrays values as keys and their occurrence count as the value
+  counts = array.inject({}) { |k, v| k[v] = array.count(v); k }
 
+  # select the highest occurring hash's key value, or keys, if more than one
+  final = counts.select { |k,v| v == counts.values.max }.keys
+
+  # sort the selected values
+  final.sort
+
+  return final
+
+end
 
 
 
 # 3. Refactored Solution
+# I just went with the initial solution
 
 
+# driver code from the exercise
+# p mode([1,2,3,3])         # => [3]
+# p mode([4.5, 0, 0])       # => [0]
+# p mode([1.5, -1, 1, 1.5]) # => [1.5]
+# p mode([1,1,2,2])         # => [1,2]
+# p mode([1,2,3])           # => [1,2,3], because all occur with equal frequency
+# p mode(["who", "what", "where", "who"]) # => "who"
 
 # 4. Reflection 
+# This was a complex problem. I was glad that I could use the inject method that I learned this week.
+# I really like the power of the functions that can take a block and apply it to an array to give 
+# a result. I feel confident that I learned the compentenies of this exercise. 
+
+
