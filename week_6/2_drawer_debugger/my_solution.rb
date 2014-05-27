71559cd4b12be86c26b34e40fe5dab51bbd272da
Steve Loar <sjl7909@gmail.com>
2014-05-27 19:19:22 -0400

    Week #6 Exercise 4

--- week_6/2_drawer_debugger/my_solution.rb
+++ week_6/2_drawer_debugger/my_solution.rb
@@ -1,62 +1,75 @@
 # U2.W6: Drawer Debugger
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 
 # 2. Original Code
 
 class Drawer
 
-attr_reader :contents
+  attr_reader :contents
 
-# Are there any more methods needed in this class?
+  # Are there any more methods needed in this class?
 
-def initialize
-@contents = []
-@open = true
+  def initialize
+    @contents = []
+    @open = true
+  end
+
+  def open
+    @open = true
+  end
+
+  def close
+    @open = false
+  end 
+
+  def add_item( item )
+    @contents << item
+  end
+
+  def remove_item(item = @contents.pop) #what is `#pop` doing? 
+  	# pop is pulling the last item off the array of contents if no item passed and item is set to that.
+  	# puts item
+  	# puts @contents
+
+    @contents.delete(item) # delete the matching item from the contents if it is still there.
+
+    # puts @contents
+    return item # added this line to return the id of the item removed
+  end
+
+  def dump  # what should this method return? nothing or nil
+    puts "Your drawer is empty."
+    @contents = [] # added this line to actually empty the contents
+  end
+
+  def view_contents
+    puts "The drawer contains:"
+      @contents.each {|silverware| puts "- " + silverware.type }
+    puts "- Nothing" if @contents == []
+  end
 end
-
-def open
-@open = true
-end
-
-def close
-@open = false
-end 
-
-def add_item
-@contents << item
-end
-
-def remove_item(item = @contents.pop) #what is `#pop` doing?
-@contents.delete(item)
-end
-
-def dump  # what should this method return?
-puts "Your drawer is empty."
-end
-
-def view_contents
-puts "The drawer contains:"
-@contents.each {|silverware| puts "- " + silverware.type }
-end
-
 
 class Silverware
-attr_reader :type
+  attr_reader :type, :clean
 
-# Are there any more methods needed in this class?
+  # Are there any more methods needed in this class? No, added the clean attribute reader.
 
-def initialize(type, clean = true)
-@type = type
-@clean = clean
-end
+  def initialize(type, clean = true)
+    @type = type
+    @clean = clean
+  end
 
-def eat
-puts "eating with the #{type}"
-@clean = false
-end
+  def eat
+    puts "eating with the #{@type}"
+    @clean = false
+  end
+
+  def clean_silverware
+  	@clean = true
+  end
 
 end
 
@@ -83,20 +96,31 @@ removed_knife.clean_silverware
 
 silverware_drawer.view_contents
 silverware_drawer.dump
-silverware_drawer.view_contents #What should this return?
+silverware_drawer.view_contents #What should this return? No items should be printed. Changed the code to say nothing.
 
-
+fork = Silverware.new("fork") # This line is needed to get the fork to work
+silverware_drawer.add_item(fork)  # This line is needed to get the fork to work
 fork = silverware_drawer.remove_item(fork) #add some puts statements to help you trace through the code...
 fork.eat
 
 #BONUS SECTION
-# puts fork.clean
+puts fork.clean # the clean attribute needed to have and attribute reader in the class.
 
 # DRIVER TESTS GO BELOW THIS LINE
+def assert
+  raise "Assertion failed!" unless yield
+end
 
-
+spoon_new = Silverware.new("spoon") 
+silverware_drawer.add_item(spoon_new)
+assert { silverware_drawer.contents[0] == spoon_new } # is our spoon in the drawer the one we put in?
+spoon = silverware_drawer.remove_item(spoon_new)
+assert { spoon.clean } # is our retreived spoon clean?
+assert { silverware_drawer.contents == [] } # is the drawer empty now?
 
 
 
 
 # 5. Reflection 
+
+
--- week_6/4_PezDispenser_solo_challenge/my_solution.rb
+++ week_6/4_PezDispenser_solo_challenge/my_solution.rb
@@ -1,7 +1,7 @@
 # U2.W6: PezDispenser Class from User Stories
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 1. Review the following user stories:
 # - As a pez user, I'd like to be able to "create" a new pez dispenser with a group of flavors that 
@@ -15,27 +15,72 @@
 
 
 # 2. Pseudocode
-
+# create an initialize method that takes a list of flavors and stores them in the dispencer
+# define a method pez_count that returns the number of items in the dispencer
+# define a method see_all_pez that lists out the items in the dispencer
+# define a method add_pez that adds a flavor to the dispencer
+# define a method get_pez that takes a flavor from the dispencer and returns it
 
 
 # 3. Initial Solution
 
-class PezDispenser
+# class PezDispenser
  
-# your code here!
+#   def initialize( flavors )
+#   	@flavors = flavors
+#   end
+
+#   def pez_count
+#   	@flavors.length
+#   end
+
+#   def see_all_pez 
+#   	@flavors.each { |flavor| }
+#   end 
+
+#   def add_pez( flavor )
+#   	@flavors << flavor
+#   end 
+
+#   def get_pez
+#   	return @flavors.pop
+#   end
  
-end
+# end
  
 
 
 # 4. Refactored Solution
+class PezDispenser
+ 
+  def initialize( flavors )
+  	@flavors = flavors
+  end
 
+  def pez_count
+  	@flavors.length
+  end
 
+  def see_all_pez 
+  	@flavors.each { |flavor| }
+  end 
 
+  def add_pez( flavor )
+  	@flavors << flavor
+  end 
+
+  def get_pez
+  	return @flavors.pop
+  end
+ 
+end
 
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
+def assert
+  raise "Assertion failed!" unless yield
+end
 
 flavors = %w(cherry chocolate cola grape lemon orange peppermint raspberry strawberry).shuffle
 super_mario = PezDispenser.new(flavors)
@@ -49,7 +94,20 @@ puts "Oh, you want one do you?"
 puts "The pez flavor you got is: #{super_mario.get_pez}"
 puts "Now you have #{super_mario.pez_count} pez!"
 
-
+super_mario.add_pez("purple") 
+assert {super_mario.pez_count == 10}
+assert {super_mario.get_pez == "purple"}
+assert {super_mario.pez_count == 9}
 
 
 # 5. Reflection 
+# My strategy was to read the driver code provided and write pseudocode to match what it called for
+# I then started to fill in the methods and run the tests. I then filled in the methods to make the
+# tests pass. Finally I refactored a few tests to use the assert method. I did not have any questions
+# but I had to look up %w to learn that it was making an array. I also learned that doing an #each loop
+# with no block will return the split out items. I feel confident that I learned the objectives.
+# I enjoyed this exercise, as if kinda flowed onto the page. Nothing i would call tedious.
+
+
+
+
